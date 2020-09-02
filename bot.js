dc376019993fdb33b4fd5860235ea69ec3952c95
N00bKefka <N00bKefka@gmail.com>
2020-09-02 21:43:00 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,7 +7,7 @@ logger.add(new logger.transports.Console, {
 });
 logger.level = 'debug';*/
 const Discord = require("discord.js");
-//const d20 = required('d20');
+const d20 = required('d20');
 //var auth = require('./auth.json');
 
 // Initialize Discord Bot
@@ -67,14 +67,14 @@ bot.on("message", (message) => {
 	}
 	else
 	{
-		// switch(strSplit[0])
-		// {
-			// case '!r':
-			// case '!roll':
-				// let diceVerbose = message.content.substring(strSplit[0].length);
-				// d20.roll(diceVerbose);
-				// break;
-		// }
+		switch(strSplit[0])
+		{
+			case '!r':
+			case '!roll':
+				let diceVerbose = message.content.substring(strSplit[0].length);
+				d20.roll(diceVerbose);
+				break;
+		}
 	}
 });
 
